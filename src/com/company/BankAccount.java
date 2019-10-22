9e7ca8f8614f67fbd6ed796fc53f3983772135a9
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-10-22 16:56:41 +0300

    add reentrant lock

--- src/com/company/BankAccount.java
+++ src/com/company/BankAccount.java
@@ -1,16 +1,21 @@
 package com.company;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
 
     private double balance;
     private String accountNumber;
+    private Lock lock;
 
     public BankAccount(String accountNumber, double initialBalance) {
         this.balance = initialBalance;
         this.accountNumber = accountNumber;
+        this.lock = new ReentrantLock();
     }
 
-    public synchronized void deposit(double ammount){
+    /*public synchronized void deposit(double ammount){
         balance += ammount;
         System.out.println(balance);
     }
@@ -18,23 +23,36 @@ public class BankAccount {
     public synchronized void withdraw(double ammount){
         balance -= ammount;
         System.out.println(balance);
-    }
+    }*/
 
 
-    /*public void deposit(double ammount) {
-        synchronized (this) {
+    public void deposit(double ammount) {
+        lock.lock();
+        try {
             balance += ammount;
+        } finally {
+            lock.unlock();
         }
         System.out.println(balance);
     }
 
     public void withdraw(double ammount) {
-        synchronized (this) {
+        lock.lock();
+        try {
             balance -= ammount;
-
+        } finally {
+            lock.unlock();
         }
         System.out.println(balance);
-    }*/
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void printAccountNumber() {
+        System.out.println("Account number " + accountNumber);
+    }
 
     @Override
     public String toString() {
