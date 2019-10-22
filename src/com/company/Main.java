f647f5d1f142fc11fe3c09fd7c5331651937fdc3
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-10-22 15:42:40 +0300

    comment first solution

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,19 +7,19 @@ public class Main {
         final BankAccount bankAccount = new BankAccount("12345-678", 1000.00);
 
 
-        Thread trThread1 = new Thread() {
-            public void run() {
-                bankAccount.deposit(300.00);
-                bankAccount.withdraw(50.00);
-            }
-        };
-
-        Thread trThread2 = new Thread() {
-            public void run() {
-                bankAccount.deposit(203.75);
-                bankAccount.withdraw(100);
-            }
-        };
+//        Thread trThread1 = new Thread() {
+//            public void run() {
+//                bankAccount.deposit(300.00);
+//                bankAccount.withdraw(50.00);
+//            }
+//        };
+//
+//        Thread trThread2 = new Thread() {
+//            public void run() {
+//                bankAccount.deposit(203.75);
+//                bankAccount.withdraw(100);
+//            }
+//        };
 
         trThread1.start();
         trThread2.start();
